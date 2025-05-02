d56426ff07fd0e14612dd243f33215e45a07112e
Sweets Sweetman <sweetmantech@gmail.com>
2025-05-02 11:08:42 -0300

    generateMetadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,9 +9,27 @@ import { MiniKitProvider } from "@/providers/MiniKitProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "SDFM 2520 - Premium Hoodies",
-  description: "Premium streetwear and comfortable hoodies",
+export const generateMetadata = (): Metadata => {
+  return {
+    title: "SDFM 2520 - Premium Hoodies",
+    description: "Premium streetwear and comfortable hoodies",
+    other: {
+      "fc:frame": JSON.stringify({
+        version: process.env.NEXT_PUBLIC_VERSION,
+        imageUrl: process.env.NEXT_PUBLIC_IMAGE_URL,
+        button: {
+          title: `Launch ${process.env.NEXT_PUBLIC_ONCHAINKIT_PROJECT_NAME}`,
+          action: {
+            type: "launch_frame",
+            name: process.env.NEXT_PUBLIC_ONCHAINKIT_PROJECT_NAME,
+            url: process.env.NEXT_PUBLIC_URL,
+            splashImageUrl: process.env.NEXT_PUBLIC_SPLASH_IMAGE_URL,
+            splashBackgroundColor: `#${process.env.NEXT_PUBLIC_SPLASH_BACKGROUND_COLOR}`,
+          },
+        },
+      }),
+    },
+  };
 };
 
 export default function RootLayout({
